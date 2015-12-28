f921a7b0c823554c0afd0645f4525f50ca5b4649
TonyQ <tonylovejava@gmal.com>
2015-12-28 11:38:44 +0800

    finetune

--- generator.js
+++ generator.js
@@ -13,8 +13,9 @@ function truncate( value, arg ) {
 
 var items = JSON.parse(fs.readFileSync("data.json"));
 // renderFile 
+var last = null;
 
-items.forEach(function(item){
+items.forEach(function(item,ind){
   item.truncate = truncate;
   item.tag_ary = item.tags.split(",");
 
@@ -22,6 +23,10 @@ items.forEach(function(item){
     item.sn = "00"+item.index;
   }else if(item.index < 100){
     item.sn = "0"+item.index;
+  }
+
+  if(item.fanstitle == null){
+    item.fanstitle = item.name;
   }
 
   item.part1_ary = item.part1.split("\n");
@@ -46,7 +51,29 @@ items.forEach(function(item){
   });
   item.url = 'http://twunbound.github.io/view/'+item.id;
 
+  if(item.prev == null && last != null){
+    item.prev = {
+      title:last.title,
+      id:last.id
+    };
+  }
+
+  if(item.next == null && items[ind+1] != null){
+    item.next = {
+      title:items[ind+1].wait ? items[ind+1].name:items[ind+1].title,
+      id:items[ind+1].id,
+      wait:items[ind+1].wait
+    };
+  }else if(item.next.wait == false ){
+    item.next = {
+      title:items[ind+1].title,
+      id:items[ind+1].id
+    };
+  }
+
   var html = jade.renderFile('temp/user.jade', item);
   fs.writeFileSync("view/"+item.id+".html", html);
+
+  last = item;
 });
 
